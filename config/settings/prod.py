0bd231eef1605b77ee72c7e8edad4d87b5145427
gosha5063 <88386494+gosha5063@users.noreply.github.com>
2026-04-14 12:35:53 +0300

    release

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,4 +2,8 @@ from .base import *
 
 DEBUG = False
 
+# Serve static files from a directory that nginx (www-data) can read.
+# Keeping it out of /home avoids common 403 permission issues.
+STATIC_ROOT = Path(os.getenv('STATIC_ROOT', '/var/www/axelot/static'))
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
